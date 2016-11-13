0c31490b46ea9297ba60976459dada24b369389a
potatoirl <howishoopwoop@gmail.com>
2016-11-13 10:53:18 -0500

    initial commit - working audio/step sync.

--- android/src/com/phfl/stepmandroid/AndroidLauncher.java
+++ android/src/com/phfl/stepmandroid/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.phfl.stepmandroid.StepmandroidGame;
+import com.phfl.stepmandroid.SMApplication;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new StepmandroidGame(), config);
+		initialize(new SMApplication(), config);
 	}
 }
--- desktop/src/com/phfl/stepmandroid/desktop/DesktopLauncher.java
+++ desktop/src/com/phfl/stepmandroid/desktop/DesktopLauncher.java
@@ -2,11 +2,13 @@ package com.phfl.stepmandroid.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.phfl.stepmandroid.StepmandroidGame;
+import com.phfl.stepmandroid.SMApplication;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new StepmandroidGame(), config);
+		config.width = 600;
+		config.height = 800;
+		new LwjglApplication(new SMApplication(), config);
 	}
 }
